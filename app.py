ca2e5da46ee99b5b791c4296b67b2043dcbb30de
Ujjwal Gupta <ujjwalgpt204@gmail.com>
2026-07-23 11:26:26 +0530

    Module 1: Project setup completed

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="AttendMitra AI",
+    page_icon="🎓",
+    layout="wide"
+)
+
+st.title("🎓 AttendMitra AI")
+
+st.subheader("AI Powered Attendance Management System")
+
+st.success("Module 1 Setup Completed Successfully!")
